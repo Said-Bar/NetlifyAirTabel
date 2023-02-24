79429c296466b245dbc469b2b8fbd7cf762e942d
Jura Krivoshey <jurakrivoshey@gmail.com>
2023-02-25 00:36:14 +0200

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,9 +7,9 @@ const Hero = () => {
   return (
     <Wrapper className='section-center'>
       <article className='content'>
-        <h1>
-          design your <br />
-          comfort zone
+        <h1>Steel horses
+           <br />
+           of a bygone era
         </h1>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at
