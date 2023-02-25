1a77fde10ffb127e9f568fb719a9ec126dfc509e
Jura Krivoshey <jurakrivoshey@gmail.com>
2023-02-25 12:09:41 +0200

    first commit

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -165,7 +165,7 @@ const Wrapper = styled.section`
       0px 2px 5px 0px rgba(50, 50, 93, 0.1),
       0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
     border-radius: 7px;
-    padding: 40px;
+    padding: 20px;
   }
   input {
     border-radius: 6px;
@@ -292,7 +292,7 @@ const Wrapper = styled.section`
   }
   @media only screen and (max-width: 600px) {
     form {
-      width: 80vw;
+      width: 99vw;
     }
   }
 `
